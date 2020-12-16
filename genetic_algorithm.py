369bb4c467c8754b367ab0a7e99ed0ae061828a7
Chriscrosser3310 <3042109025@qq.com>
2020-12-15 18:37:46 -0800

    Add selection, crossover and mutation

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -2,8 +2,41 @@ import random
 from LightsOutBoard import LightsOutBoard1000x1000, LightsOutBoard8x8, LightsOutBoard4x3, LightsOutBoard5x5
 
 class GeneticAlgorithm:
-  def __init__(self, init_population: ['LightsOutBoardnxm']):
+  def __init__(self, board: 'LightsOutBoardnxm', init_population: ['LightsOutBoardnxm']):
+    self.population_size = len(init_population)
+    if self.population_size % 2 != 0:
+      raise ValueError('Population size must be an even number.')
+    self.population = init_population
+    self.board_size = board.getNumParameters()
+
+  def _selection(threshold: float):
+    fitted_parents = []
+    for p in self.population:
+      if board.getCost(p) <= threshold:
+        new_population.append(p)
+    return fitted_parents
+
+  def _cross_over(parents):
+    offsprings = []
+    num_parents = len(parents)
+    while len(offsprings) < self.population_size:
+      mom, dad = random.sample(parents, 2)
+      cross_pos = random.randrange(0, self.board_size)
+      offsprings.append(mom.single_crossover(dad, cross_pos))
+      offsprings.append(dad.single_crossover(mom, cross_pos))
+    return offsprings
+
+
+  def _mutation(offsprings):
+    num_mutation = random.randrange(0, self.population_size)
+    to_be_mutated = random.sample(offsprings, num_mutation)
+    for m in to_be_mutated:
+      m.mutate_random(random.uniform(0, 0.1))
+
+  def run_once():
     pass
+
+
 
 if __name__ == '__main__':
   # TEST
